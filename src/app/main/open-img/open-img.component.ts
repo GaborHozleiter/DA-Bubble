fda3a6823b2e70229a79682981d020734cbda8ef
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-11-07 20:11:45 +0100

    updates from 07.11.2024

--- src/app/main/open-img/open-img.component.ts
+++ src/app/main/open-img/open-img.component.ts
@@ -1,48 +1,74 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ChannelSelectionService } from '../../services/channel-selection.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-open-img',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule], // Füge HttpClientModule hier hinzu
   templateUrl: './open-img.component.html',
-  styleUrl: './open-img.component.scss',
+  styleUrls: ['./open-img.component.scss'],
 })
 export class OpenImgComponent implements OnInit {
   src = '';
-  constructor(public channelSelectionService: ChannelSelectionService) {}
+  constructor(
+    public channelSelectionService: ChannelSelectionService,
+    private http: HttpClient
+  ) {}
+
+  imgWidth: any;
+  imgHeight: any;
 
   ngOnInit(): void {
     this.channelSelectionService.getselectedIMG().subscribe((src) => {
       this.src = src;
+      if (src) {
+        this.getImageDimensionBasedOnAspect(src);
+      }
     });
   }
 
-  /**
-   * Stops the propagation of the specified event, preventing it from bubbling up to parent elements.
-   *
-   * @param {any} event - The event object to stop propagation for.
-   * @returns {void}
-   */
   stopEvent(event: any): void {
     event.stopPropagation();
   }
 
-  /**
-   * Determines whether the provided image source has a greater width or height based on its aspect ratio.
-   *
-   * @param {string} src - The source URL of the image.
-   * @returns {'width' | 'height'} - Returns 'width' if the image is wider than it is tall, otherwise returns 'height'.
-   */
-  getImageDimensionBasedOnAspect(src: string): 'width' | 'height' {
+  getImageDimensionBasedOnAspect(src: string) {
     const img = new Image();
     img.src = src;
+    const width = img.width;
+    const height = img.height;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-    if (img.width > img.height) {
-      return 'width';
-    } else {
-      return 'height';
-    }
+    const maxWidth = screenWidth * 0.8;
+    const maxHeight = screenHeight * 0.8;
+
+    let newWidth = width;
+    let newHeight = height;
+
+    const widthRatio = maxWidth / width;
+    const heightRatio = maxHeight / height;
+    const scalingFactor = Math.min(widthRatio, heightRatio);
+
+    newWidth = width * scalingFactor;
+    newHeight = height * scalingFactor;
+
+    this.imgHeight = newHeight;
+    this.imgWidth = newWidth;
+  }
+
+  downloadImg(src: string) {
+    this.http.get(src, { responseType: 'blob' }).subscribe((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+
+      const fileName = src.split('?')[0].split('/').pop() || 'download.jpg';
+      a.download = fileName;
+      a.click();
+
+      window.URL.revokeObjectURL(url);
+    });
   }
 }
